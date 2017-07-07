88a790aa33e65731881682ff4fd967ee4871de46
Margaret Walker <mwalker@Margarets-MacBook-Pro.local>
2017-07-07 16:26:10 -0400

    Finished part 5 of tutorial. Testing is frustrating, but useful!

--- polls/views.py
+++ polls/views.py
@@ -7,6 +7,7 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -15,18 +16,27 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published questions excluding those to be published
+        in the future."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
-
+    def get_queryset(self):
+            """
+            excludes any quesitons that aren't published yet
+            """
+            return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
